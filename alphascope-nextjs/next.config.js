f383641097cd4d476ea6c9662146dd274ae48b4a
perebicovschiofficial <159610755+perebicovschiofficial@users.noreply.github.com>
2026-04-20 17:44:13 +0100

    Update next.config.js

--- alphascope-nextjs/next.config.js
+++ alphascope-nextjs/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'ru', 'zh', 'es', 'ar', 'fr'],
+    defaultLocale: 'en',
+  },
 };
 
 module.exports = nextConfig;
